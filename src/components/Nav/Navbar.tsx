7d4b61349e34b1d5daa25c2b2f9c3a1d071b4f62
Nevacat <nevacattery217@naver.com>
2024-03-03 16:11:28 +0900

    feat:Navbar 생성

---
+++ src/components/Nav/Navbar.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { cn } from "@/lib/util";
+import React, { useState } from "react";
+import NavItem from "./NavItem";
+import Link from "next/link";
+
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const handleMenu = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  return (
+    <nav className={cn("relative z-10 w-full bg-[#535C91] text-white")}>
+      <div className={cn("flex items-center justify-between mx-5 sm:mx-10 lg:mx-20")}>
+        <div className={cn("flex items-center text-2xl h-14")}>
+          <Link href="/">로드마켓</Link>
+        </div>
+        <div className={cn("text-2xl sm:hidden")}>
+          {!navbarOpen ? <div onClick={handleMenu}>+</div> : <div onClick={handleMenu}>-</div>}
+          </div>
+        <div className={cn("hidden sm:block")}>
+          <NavItem />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
