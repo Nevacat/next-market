fc3a94f2393563477094d1722cbe84c4e94ce3b3
Nevacat <nevacattery217@naver.com>
2024-03-08 01:23:16 +0900

    feat:로그인 버튼 클릭 시 login 페이지로 이동 설정

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -31,6 +31,9 @@ export const authOptions : NextAuthOptions = {
     maxAge: 30 * 24 * 60 * 60,
   },
   secret: process.env.JWT_SECRET,
+  pages:{
+    signIn: "/auth/login",
+  },
   callbacks:{
     async jwt({token, user}){
       return {...token, ...user}
