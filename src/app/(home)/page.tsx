48a511abc271d4b62902cc86557d612ff2d64a83
Nevacat <nevacattery217@naver.com>
2024-03-11 02:21:03 +0900

    feat:Floating 버튼 추가

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,11 +4,13 @@ import React from 'react'
 import getCurrentUser from '../actions/getCurrentUser'
 import { user } from '@/atom/user'
 import { useSetRecoilState } from 'recoil'
+import FloatingButton from '@/components/FloatingButton'
 
 const MainPage = () => {
   return (
     <Container>
       <div>메인</div>
+      <FloatingButton href='/products/upload'>+</FloatingButton>
     </Container>
   )
 }
