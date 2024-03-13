a43581f23595a1534abcbd2fa99a55f668a9ec5d
Nevacat <nevacattery217@naver.com>
2024-03-13 19:50:29 +0900

    feat: current유저 타입 생성

--- src/app/actions/getCurrentUser.ts
+++ src/app/actions/getCurrentUser.ts
@@ -2,8 +2,21 @@
 import { authOptions } from "@/pages/api/auth/[...nextauth]"
 import { getServerSession } from "next-auth"
 import prisma from "@/helper/prismadb"
+
+
 export async function getSession(){
   return await getServerSession(authOptions)
+}
+
+export interface CurrentUser {
+  id:string,
+  email:string,
+  username:string,
+  role:string,
+  favorites:string[],
+  products:string[],
+  createdAt:string,
+  updatedAt:string,
 }
 
 const getCurrentUser = async () => {
@@ -27,11 +40,9 @@ const getCurrentUser = async () => {
         updatedAt:true,
       }
     })
-    console.log(current)
     if(!current){
       return null
     }
-    console.log(current)
     return current
   }catch(err){
     console.log(err)
