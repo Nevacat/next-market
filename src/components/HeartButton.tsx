b82ce4fd340b2a01210ae472865b6b228036583a
Nevacat <nevacattery217@naver.com>
2024-03-13 19:51:11 +0900

    feat: 좋아요 버튼 구현

---
+++ src/components/HeartButton.tsx
@@ -0,0 +1,40 @@
+import useFavorite from '@/hooks/useFavorite'
+import { cn } from '@/lib/util'
+import { User } from '@prisma/client';
+import React from 'react'
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
+
+interface HeartButtonProps{
+  productId:string;
+  currentUser?:User|null
+}
+
+const HeartButton = ({productId,currentUser}:HeartButtonProps) => {
+
+  const {hasFavorite,toggleFavorite} = useFavorite({
+    productId,currentUser
+  })
+
+  return (
+    <div
+      onClick={toggleFavorite}
+      className="relative transition cursor-pointer hover:opacity-80"
+    >
+      <AiOutlineHeart
+        size={28}
+        className="
+          absolute
+          fill-white
+          -top-[2px]
+          -right-[2px]
+        "
+      />
+      <AiFillHeart
+        size={24}
+        className={cn(hasFavorite?"fill-rose-500":"fill-neutral-500")}
+      />
+    </div>
+  )
+}
+
+export default HeartButton
---
+++ src/hooks/useFavorite.tsx
@@ -0,0 +1,40 @@
+import { CurrentUser } from "@/app/actions/getCurrentUser";
+import { User } from "@prisma/client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useMemo } from "react";
+
+interface useFavoriteProps {
+  productId: string;
+  currentUser?: CurrentUser | null;
+}
+
+const useFavorite = ({ productId, currentUser }: useFavoriteProps) => {
+  const router = useRouter();
+  const hasFavorite = useMemo(() => {
+    const list: string[] = currentUser?.favorites || [];
+    return list.includes(productId);
+  }, [currentUser, productId]);
+
+  const toggleFavorite = async (e:React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation()
+    try {
+      let request;
+      if (hasFavorite) {
+        request = () => axios.delete(`/api/favorite/${productId}`);
+      } else {
+        request = () => axios.post(`/api/favorite/${productId}`);
+      }
+      await request();
+      router.refresh()
+    } catch (err: any) {
+      throw new Error(err);
+    }
+  };
+  return {
+    hasFavorite,
+    toggleFavorite,
+  };
+};
+
+export default useFavorite;
