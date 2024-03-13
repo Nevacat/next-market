e5cdd9dee06cce408624cf065ee24530110f4962
Nevacat <nevacattery217@naver.com>
2024-03-13 19:51:52 +0900

    delete:console.log제거

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,6 @@ import React from 'react'
 export async function middleware(req: NextRequest) {
   const session = await getToken({req, secret: process.env.JWT_SECRET})
   const pathname = req.nextUrl.pathname
-  console.log(req.url)
   if(pathname.startsWith('/admin') && !session){
     return NextResponse.redirect(new URL('/auth/login',req.url))
   }
