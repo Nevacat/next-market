23e333b99ed44d71af9d6a70a07802c0dc5e39fc
Nevacat <nevacattery217@naver.com>
2024-03-13 19:50:16 +0900

    feat:목록 페이지 네이션 구현

---
+++ src/components/PaginationLink.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import React, { PropsWithChildren } from 'react';
+import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
+import queryString from 'query-string';
+
+type PaginationLinkProps = {
+    page?: number;
+    disabled?: boolean;
+    active?: boolean;
+} & PropsWithChildren
+
+const PaginationLink = ({ page, disabled, active, children }: PaginationLinkProps) => {
+    const params = useSearchParams();
+    const limit = 12;
+    const skip = page ? (Number(page) - 1) * limit : 0; 
+
+    let currentQuery = {};
+    if(params) {
+        currentQuery = queryString.parse(params?.toString())
+    }
+    const updatedQuery = {
+        ...currentQuery, 
+        page,
+        skip
+    }
+
+  return (
+    <Link 
+      href={{ query: updatedQuery }} 
+      className={`p-2 text-2xl 
+      ${active ? "font-bold text-[#535C91]" :""}
+      ${disabled ? "pointer-events-none text-gray-200" : ""}
+      `}
+    >{children}</Link>
+  )
+}
+
+export default PaginationLink
