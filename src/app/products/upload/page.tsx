fce49f49f616b58cdfcd459c1164ac7f22830429
Nevacat <nevacattery217@naver.com>
2024-03-13 19:48:06 +0900

    feat:업로드 페이지 카테고리 추가

--- src/app/products/upload/page.tsx
+++ src/app/products/upload/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 import Button from '@/components/Button'
+import { categories } from '@/components/Category/Categories'
+import CategoryInput from '@/components/Category/CategoryInput'
 import Container from '@/components/Container'
 import Heading from '@/components/Heading'
 import ImageUpload from '@/components/ImageUpload'
@@ -20,7 +22,7 @@ const ProductUpload = () => {
       description:'',
       price:1,
       imageSrc:'',
-      category:'',
+      category:'digital',
       latitude:37.5716,
       longitude:126.9766586,
     }
@@ -29,6 +31,7 @@ const ProductUpload = () => {
   const ImageSrc = watch('imageSrc')
   const Latitude = watch('latitude')
   const Longitude = watch('longitude')
+  const category = watch('category')
   const KakaoMap = dynamic(()=>import('../../../components/Kakaomap'),{ssr:false})
   const setCustomValue = (id:string,value:any) => {
     setValue(id,value)
@@ -47,7 +50,6 @@ const ProductUpload = () => {
         setIsLoading(false);
       })
   }
-
   return (
     <Container>
       <div className='max-w-screen-lg mx-auto'>
@@ -62,6 +64,28 @@ const ProductUpload = () => {
           <Input id='name' type='text' label='상품명' disabled={isLoading} register={register} errors={errors} required={true}/>
           <Input id='description' type='text' label='상품설명' disabled={isLoading} register={register} errors={errors} required={true}/>
           <Input id='price' type='text' label='가격' formatprice disabled={isLoading} register={register} errors={errors} required={true}/>
+          <div
+            className='grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto'
+          >
+            {
+              categories.map((item)=>(
+                <div
+                  key={item.label}
+                  className="
+                  "
+                >
+                  <CategoryInput
+                    onClick={(category)=>{
+                      setCustomValue('category',category)}}
+                    selected={category===item.path}
+                    label={item.label}
+                    icon={item.icon}
+                    path={item.path}
+                  />
+                </div>
+              ))
+            }
+          </div>
           <KakaoMap latitude={Latitude} longitude={Longitude} setCustomValue={setCustomValue} />
           <Button text="업로드" onClick={handleSubmit(onSubmit)}/>
         </form>
