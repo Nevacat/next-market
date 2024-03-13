d5d7c1ca7430931ee6ea958fefc2593a38360aed
Nevacat <nevacattery217@naver.com>
2024-03-13 19:51:25 +0900

    fixed: 타입 에러 수정

--- src/atom/user.ts
+++ src/atom/user.ts
@@ -3,12 +3,13 @@ import { atom } from "recoil";
 export const user = atom({
   key: 'user',
   default: {
+    id:'',
     email: '',
     username: '',
-    favorites: [],
-    products: [],
+    favorites: [''],
+    products: [''],
     role: '',
     createdAt: '',
-    updatedAt: ''
+    updatedAt: '',
   }
 })
