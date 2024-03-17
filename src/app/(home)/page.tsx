c1bdf8f23b1a1f5e14ed06e5b27668574c91ec45
Nevacat <nevacattery217@naver.com>
2024-03-18 00:50:19 +0900

    feat:제품이 없을 때 empty 추가

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,51 +1,42 @@
-import Container from '@/components/Container'
-import React from 'react'
-import getCurrentUser from '../actions/getCurrentUser'
-import { user } from '@/atom/user'
-import { useSetRecoilState } from 'recoil'
-import FloatingButton from '@/components/FloatingButton'
-import Categories from '@/components/Category/Categories'
-import axios from 'axios'
-import getProducts, { ProductParams } from '../actions/getProducts'
-import ProductCard from '@/components/Products/ProductCard'
-import { Product } from '@prisma/client'
-import Pagination from '@/components/Pagination'
+import Container from "@/components/Container";
+import React from "react";
+import getCurrentUser from "../actions/getCurrentUser";
+import { user } from "@/atom/user";
+import { useSetRecoilState } from "recoil";
+import FloatingButton from "@/components/FloatingButton";
+import Categories from "@/components/Category/Categories";
+import axios from "axios";
+import getProducts, { ProductParams } from "../actions/getProducts";
+import ProductCard from "@/components/Products/ProductCard";
+import { Product } from "@prisma/client";
+import Pagination from "@/components/Pagination";
+import EmptyState from "@/components/EmptyState";
 
-interface MainProps{
-  searchParams:ProductParams
+interface MainProps {
+  searchParams: ProductParams;
 }
 
-export default async function Main({searchParams}:MainProps){
-  const page = searchParams?.page
+export default async function Main({ searchParams }: MainProps) {
+  const page = searchParams?.page;
 
-  const products = await getProducts(searchParams)
-  const total = products?.totalItems
-
+  const products = await getProducts(searchParams);
+  const total = products?.totalItems;
   return (
     <Container>
-      <Categories/>
-      {
-        <div
-          className='grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 mt-4 md:mt-6 gap-2 md:gap-8'
-        >
-          {
-            products?.data.map((product:Product)=>(
-              <ProductCard
-                key={product.id}
-                data={product}
-              />
-            ))
-          }
+      <Categories />
+      {products?.data.length === 0 ? (
+        <EmptyState />
+      ) : (
+        <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 mt-4 md:mt-6 gap-2 md:gap-8">
+          {products?.data.map((product: Product) => (
+            <ProductCard key={product.id} data={product} />
+          ))}
         </div>
-      }
-      <div className='w-full my-10 flex justify-center items-center'>
-      <Pagination
-        totalItems={total}
-        page={page}
-      />
+      )}
+      <div className="w-full my-10 flex justify-center items-center">
+        <Pagination totalItems={total} page={page} />
       </div>
-      <FloatingButton href='/products/upload'>+</FloatingButton>
+      <FloatingButton href="/products/upload">+</FloatingButton>
     </Container>
-  )
+  );
 }
-
