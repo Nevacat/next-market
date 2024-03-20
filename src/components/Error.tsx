582a67525908b2a1d40ee207df51a8356192f06f
Nevacat <nevacattery217@naver.com>
2024-03-20 19:22:30 +0900

    feat:Error컴포넌트 추가

---
+++ src/components/Error.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className="h-[80vh] max-w-6xl mx-auto w-full flex flex-col items-center justify-center py-40 text-2xl font-semibold">
+    에러가 발생했습니다.
+  </div>
+  )
+}
+
+export default Error
