e312d112128f0373ff26d82f35ca5a14c00d6554
Nevacat <nevacattery217@naver.com>
2024-03-20 14:08:23 +0900

    docs:prettier

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,36 +1,39 @@
-import NextAuth, { NextAuthOptions, Session } from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth, { NextAuthOptions, Session } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from '@/helper/prismadb'
+import prisma from "@/helper/prismadb";
 import { Adapter } from "next-auth/adapters";
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
 
-export const authOptions : NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     CredentialsProvider({
       name: "Credentials",
       credentials: {
         email: { label: "Email", type: "text", placeholder: "Road@test.com" },
-        password: { label: "Password", type: "password" }
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        if(!credentials?.email || !credentials?.password){
-          throw new Error("이메일 또는 비밀번호를 입력해주세요.")
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("이메일 또는 비밀번호를 입력해주세요.");
         }
         const user = await prisma.user.findUnique({
-          where:{
-            email:credentials.email
-          }
-        })
-        if(!user){
-          throw new Error("이메일 또는 비밀번호를 확인해주세요.")
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user) {
+          throw new Error("이메일 또는 비밀번호를 확인해주세요.");
         }
-        const isValid = await bcrypt.compare(credentials.password, user.password)
-        if(!isValid){
-          throw new Error("이메일 또는 비밀번호를 확인해주세요.")
+        const isValid = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+        if (!isValid) {
+          throw new Error("이메일 또는 비밀번호를 확인해주세요.");
         }
-        return user
+        return user;
       },
     }),
   ],
@@ -39,17 +42,17 @@ export const authOptions : NextAuthOptions = {
     maxAge: 24 * 60 * 60,
   },
   secret: process.env.NEXTAUTH_SECRET,
-  pages:{
+  pages: {
     signIn: "/auth/login",
   },
-  callbacks:{
-    async jwt({token, user}){
-      return {...token, ...user}
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
     },
-    async session({session, token}){
-      session.user = token as Session["user"]
-      return session
-    }
-  }
-}
-export default NextAuth(authOptions)
+    async session({ session, token }) {
+      session.user = token as Session["user"];
+      return session;
+    },
+  },
+};
+export default NextAuth(authOptions);
