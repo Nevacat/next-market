2d3ca5445b1b02d580b3a2bfed97fe943bd3e0fc
Nevacat <nevacattery217@naver.com>
2024-04-04 16:16:14 +0900

    fixed: 코드 원상복구

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,3 @@
-'use client'
 import Container from "@/components/Container";
 import React, { use, useEffect, useState } from "react";
 import getCurrentUser from "../actions/getCurrentUser";
@@ -17,19 +16,18 @@ interface MainProps {
   searchParams: ProductParams;
 }
 
-export default function Main({ searchParams }: MainProps) {
+export default async function Main({ searchParams }: MainProps) {
   const page = searchParams?.page;
-  // const products = await fetch(`${process.env.NEXT_PAGE_URL}/api/products`,{cache:"force-cache"}).then(res=>res.json());
-  const [products,setProducts] = useState([]);
-  // const total = products?.totalItems;
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch(`/api/products`).then(res=>res.json());
-      console.log(res)
-      setProducts(res);
-    };
-    fetchData();
-  }, [searchParams]);
+  const products = await fetch(`${process.env.NEXT_PAGE_URL}/api/products`,{cache:"no-store"}).then(res=>res.json());
+  // const [products,setProducts] = useState([]);
+  const total = products?.totalItems;
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch(`/api/products`).then(res=>res.json());
+  //     setProducts(res);
+  //   };
+  //   fetchData();
+  // }, [searchParams]);
   return (
     <Container>
       <Categories />
@@ -46,7 +44,7 @@ export default function Main({ searchParams }: MainProps) {
             ))}
           </div>
           <div className="w-full my-10 flex justify-center items-center">
-            <Pagination totalItems={10} page={page} />
+            <Pagination totalItems={total?total:0} page={page} />
           </div>
         </>
       )}
