1b4a370fdf73a28cc89529daa9cc8da7c9a46e6e
박희수 <110139098+Nevacat@users.noreply.github.com>
2024-04-16 15:08:55 +0900

    Update useFavorite.tsx

--- src/hooks/useFavorite.tsx
+++ src/hooks/useFavorite.tsx
@@ -1,20 +1,23 @@
 import { CurrentUser } from "@/app/actions/getCurrentUser";
+import { like } from "@/atom/user";
 import { User } from "@prisma/client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import React, { useMemo } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 
 interface useFavoriteProps {
   productId: string;
-  currentUser?: User | null;
 }
 
-const useFavorite = ({ productId, currentUser }: useFavoriteProps) => {
+const useFavorite = ({ productId}: useFavoriteProps) => {
   const router = useRouter();
+  const likes = useRecoilValue(like);
+  const setLikes = useSetRecoilState(like);
   const hasFavorite = useMemo(() => {
-    const list: string[] = currentUser?.favorites || [];
+    const list: string[] = likes || [];
     return list.includes(productId);
-  }, [currentUser, productId]);
+  }, [likes, productId]);
 
   const toggleFavorite = async (e:React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation()
@@ -25,8 +28,8 @@ const useFavorite = ({ productId, currentUser }: useFavoriteProps) => {
       } else {
         request = () => axios.post(`/api/favorite/${productId}`);
       }
-      await request();
-      router.refresh()
+      const res = await request();
+      setLikes(res.data.favorites);
     } catch (err: any) {
       err.response.status === 401 && router.push("/auth/login");
     }
